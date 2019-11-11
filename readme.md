8221b6b8dce565de803b1c2deecbc8e7762f3a5a
akhundrabbani <57610870+akhundrabbani@users.noreply.github.com>
2019-11-11 11:11:05 +0530

    AWS Terraform files

---
+++ readme.md
@@ -0,0 +1,23 @@
+#Terraform Code Blocks
+
+Provider
+Resource
+Provisioner
+Variable
+Output
+
+#Terraform Commands
+
+$terraform version
+$terraform init
+$terraform plan
+$terraform apply
+$terraform show
+$terraform destroy
+
+
+#Scenario #1
+
+Deploy AWS EC2 instance in default VPC
+Create Security Group and allow port 22 & 80
+Run Ansible Playbook to install and configure Apache httpd
---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+###############################################
+variable "access_key" {
+    type = "string"
+    default = "AKIA4SWKLIWMK3SDXOTN"
+}
+
+variable "secret_key" {
+    type = "string"
+    default = "LLV8tQmQ8GkJ08p3szuaeKpaR72779vsrCheSFOv"
+}
+
+variable "region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "instance_type" {
+    type = "string"
+    default = ""
+}
